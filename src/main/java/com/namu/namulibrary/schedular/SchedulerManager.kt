ba3205ca7c7018642a71326523d81e1c646368e2
namu <reona7140@naver.com>
2021-03-08 01:06:07 +0900

    add: 스케쥴러 긴급 중단 및 틱, 초단위 함수 분리

--- src/main/java/com/namu/namulibrary/schedular/SchedulerManager.kt
+++ src/main/java/com/namu/namulibrary/schedular/SchedulerManager.kt
@@ -2,6 +2,7 @@ package com.namu.namulibrary.schedular
 
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitTask
 
 abstract class SchedulerManager {
 
@@ -17,12 +18,31 @@ abstract class SchedulerManager {
     abstract fun doing()
     abstract fun finished()
 
-    fun run(period: Long, cycle: Int) {
+    lateinit var aBukkitTask : BukkitTask
+    lateinit var bBukkitTask : BukkitTask
+
+    fun stopScheduler() {
+        aBukkitTask.cancel()
+        bBukkitTask.cancel()
+        finished()
+    }
+
+    fun runSecond(period: Long, cycle: Int) {
         started()
-        val bukkitTask = Bukkit.getScheduler().runTaskTimer(plugin, Runnable { doing() }, 0L, period * 20)
-        Bukkit.getScheduler().runTaskLater(plugin, Runnable {
-            bukkitTask.cancel()
+        aBukkitTask = Bukkit.getScheduler().runTaskTimer(plugin, Runnable { doing() }, 0L, period * 20)
+        bBukkitTask = Bukkit.getScheduler().runTaskLater(plugin, Runnable {
+            aBukkitTask.cancel()
             finished()
         }, period * cycle * 20)
     }
+
+    fun runTick(period: Long, cycle: Int) {
+        started()
+        aBukkitTask = Bukkit.getScheduler().runTaskTimer(plugin, Runnable { doing() }, 0L, period * 20)
+        bBukkitTask = Bukkit.getScheduler().runTaskLater(plugin, Runnable {
+            aBukkitTask.cancel()
+            finished()
+        }, period * cycle)
+    }
+
 }
